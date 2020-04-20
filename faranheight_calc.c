c6f4f3cf53dfff30cb353e79c8f00db907b8a7b1
Logan Murphy <loganmurphy@Logans-iMac.local>
2020-04-19 19:47:49 -0500

    add header to table, add commandline args to either print C --> F or F --> C

--- faranheight_calc.c
+++ faranheight_calc.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 /* print Faranheit-celsius table for fahr = 0 ... 300 */
+char f[] = "F";
+char c[] = "C";
+char c_to_f[] = "C -> F";
+char f_to_c[] = "F -> C";
 
-int main() {
+void printRow(char standard[], float celcius, float fahr) {
+   if(strcmp(standard, f) == 0) {
+       printf("%3.0f\t\%6.1f\n", fahr, celcius);
+   } else {
+       printf("%3.0f\t\%6.1f\n", celcius, fahr);
+   }
+}
+
+int main(int argc, char *argv[]) {
     float fahr, celcius;
+    printf("%s", *argv);
     int lower, upper, step;
   
     /* lower limit of temperature table */
@@ -16,10 +30,18 @@ int main() {
     step = 20;
 
     fahr = lower;
+    
+    char *conversion;    
+    if(strcmp(argv[2], f) == 0) {
+        conversion = f_to_c;
+    } else {
+        conversion = c_to_f;
+    }
 
+    printf("\n%s Temperature Conversion Table\n", conversion);
     while (fahr <= upper) {
         celcius = (5.0/9.0) * (fahr - 32.0);
-        printf("%3.0f\t\%6.1f\n", fahr, celcius);,
+        printRow(argv[2], celcius, fahr);
         fahr = fahr + step;
     }
 }
